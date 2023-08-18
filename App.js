2c3b5dc96f60cd8f863c1fd66dbf0ff612fecfd2
Iliyas Khan <iliyaskhanh94@gmail.com>
2023-08-18 23:29:19 +0530

    fixed the App.js code

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
+const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "h1 tag"),
     React.createElement("h1", {}, "h1 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h1", {}, "h1 tag"),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
